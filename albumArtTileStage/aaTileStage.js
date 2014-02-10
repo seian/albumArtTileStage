23a7a9d83d4af5611653f567b3b1927974adccd0
Lim Byunghoon <seian@me.com>
2014-02-10 21:11:05 +0900

    modified:   .aaTileStage.js.swp 	modified:   aaTileStage.js 	modified:   .aaTileStage.js.swp 	deleted:    tileStage.js

--- albumArtTileStage/aaTileStage.js
+++ albumArtTileStage/aaTileStage.js
@@ -4,51 +4,51 @@
 // Feb., 2014
 
 $(document).ready(function(e) {
-	// album arts
-	var aa = {'column'      : 3,  		// set column
-			  'row'         : 9,  		// set row
-			  'size'        : 80, 		// set a size in pixel
-			  'bigImg'		: 3,  		// set a number of big images
-			  'mainDivId'   : 'aaSet',  // set main div for album arts section
-			  'imgAddr'     : [], 		// set image addresses
-			  'debugMode'   : true, 	// debug mode
-			  // private variables
-			  'pVars'	: [],
-			  // initializer
-			  init : function() {
-				  		this.pVars['bigImgCoord'] = [];
+    // album arts
+    var aa = {'column'      : 3,        // set column
+              'row'         : 9,        // set row
+              'size'        : 80,       // set a size in pixel
+              'bigImg'      : 3,        // set a number of big images
+              'mainDivId'   : 'aaSet',  // set main div for album arts section
+              'imgAddr'     : [],       // set image addresses
+              'debugMode'   : true,     // debug mode
+              // private variables
+              'pVars'   : [],
+              // initializer
+              init : function() {
+                        this.pVars['bigImgCoord'] = [];
 
-			  		  },
-			  // draw a table
-			  onDraw : function() {
-							
-			  		  },
-			  // get random coordination for big imgs
-			  getRandomCoordBI : function() {
-						for(var i=0;i<this.bigImg;i++) {
-							this.pVars['bigImgCoord'].push(this.setRandomCalib(i));
-						}
-			  			if(this.debugMode == true) {
-							console.log('big image coordinations :');
-			  				console.log(this.pVars['bigImgCoord']);	
-						}
-					 },
-			  // get random number
-			  setRandomCalib : function(nth) {
-						var coord = {};
-						
-						coord['_col'] = this.getRandom(this.column);
+                      },
+              // draw a table
+              onDraw : function() {
+                            
+                      },
+              // get random coordination for big imgs
+              getRandomCoordBI : function() {
+                        for(var i=0;i<this.bigImg;i++) {
+                            this.pVars['bigImgCoord'].push(this.setRandomCalib(i));
+                        }
+                        if(this.debugMode == true) {
+                            console.log('big image coordinations :');
+                            console.log(this.pVars['bigImgCoord']); 
+                        }
+                     },
+              // get random number
+              setRandomCalib : function(nth) {
+                        var coord = {};
+                        
+                        coord['_col'] = this.getRandom(this.column);
 
-						return coord;
-					 },
-			  getRandom : function(i) {
-					 	return Math.floor(Math.random() * (i-1));
-					 },
-			  exec : function() {
-						this.getRandomCoordBI();
-					 }
-			 };
-			
-	aa.init();
-	aa.getRandomCoordBI();
+                        return coord;
+                     },
+              getRandom : function(i) {
+                        return Math.floor(Math.random() * (i-1));
+                     },
+              exec : function() {
+                        this.getRandomCoordBI();
+                     }
+             };
+            
+    aa.init();
+    aa.getRandomCoordBI();
 });
